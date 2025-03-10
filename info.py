85dcc199dc2f37352df1a24b47fbd3f8fd8446ff
Vikash Chaurasiya <120702683+provikash@users.noreply.github.com>
2025-03-10 14:41:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '2001aashish@axisb')
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
 # ============================
